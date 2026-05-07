b3234348f310c834bf1616d00e0d59ac5d5dbdfa
Mysr taseety <mysrtaseety@gmail.com>
2026-05-07 18:13:57 +0200

    Implement calculateTotals function for financial summary
    Calculate total sales, expenses, net, and missing amounts.

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,49 @@
 
+//from home android
+function calculateTotals() {
+
+  state.summary.salesTotal =
+
+    state.sales.reduce((acc, sale) => {
+      return acc + sale.total;
+    }, 0);
+
+
+
+  const allExpenses = [
+
+    ...state.fixedExpenses,
+
+    ...state.dailyExpenses
+
+  ];
+
+
+
+  state.summary.expensesTotal =
+
+    allExpenses.reduce((acc, exp) => {
+      return acc + exp.amount;
+    }, 0);
+
+
+
+  state.summary.net =
+
+    state.summary.salesTotal
+    - state.summary.expensesTotal;
+
+
+
+  const actualMoney =
+
+    state.summary.cash
+    + state.summary.bank;
+
+
+
+  state.summary.missing =
+
+    actualMoney
+    - state.summary.net;
+}
