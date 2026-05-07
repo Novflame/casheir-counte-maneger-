ae33d839be6a5a913d323510017b884d9b314d18
Mysr taseety <mysrtaseety@gmail.com>
2026-05-07 18:11:37 +0200

    Add initial state object for application

--- js/state.js
+++ js/state.js
@@ -1,1 +1,28 @@
 // from android 
+const state = {
+
+  isSetupComplete: false,
+
+  products: [],
+
+  fixedExpenses: [],
+
+  sales: [],
+
+  dailyExpenses: [],
+
+  summary: {
+
+    salesTotal: 0,
+
+    expensesTotal: 0,
+
+    net: 0,
+
+    cash: 0,
+
+    bank: 0,
+
+    missing: 0
+  }
+};
